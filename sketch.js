b3d93c5281e354443c87a0463b638130830f17a3
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2021-04-06 21:11:16 +0530

    Finished game v1

--- sketch.js
+++ sketch.js
@@ -1,31 +1,26 @@
 /*
-Hangman Game
+Drownman Game
 */
 
 
 let mode = 1;
 //mode = 1 corresponds to the landing screen.
+//mode = 1.5 corresponds to the animation at the beginning of the actual game.
 //mode = 2 corresponds to the actual game.
 //mode = 3 corresponds to the animation with rising water when a wrong key is pressed.
-//mode = 3 corresponds to the animation with dancing monkeys when a correct key is pressed.
+//mode = 4 corresponds to the animation with dancing monkeys when a correct key is pressed.
+//mode = 5 corresponds to when the game is lost and player has drowned.
+//mode = 6 corresponds to when the game is won and player has guessed the word.
 let words = []; //to store all words from the txt file.
 let word; //to store the selected random word.
-let spriteSheet; //spritesheet image of a dancing monkey.
-let spriteData; //spritesheet data for dividing individual images.
-let monkeyImgs = []; //for storing extracted individual images of monkey sprite.
-let charImgs = []; //for storing extracted individual images of character sprite.
-let charIndex = 0; //for animation.
-let monkeyIndex = 0; //for animation.
-let startButton; //for starting the game
 let input; //for entering in the name
 let nameOfPlayer; //to store the name entered in the welcome screen
-let heightOfWater;
-let prevHeight;
-let wordChars = [];
-let selectedCorrectedChars = [];
-let wrongAttempts = [];
-let charHeight;
-const maxHeight = 80;
+let wordChars = []; //to store the characters of the word 
+let rightAttempts = []; //to store the correct characters of the word guessed so far
+let wrongAttempts = []; //to store the incorrect characters guessed so far
+let attemptsLeft; //to store the number of attempts left
+let charY; //to store the Y position at which the character is drawn 
+const maxHeight = 80; //just a constant number used for drawing.
 const waterHeights = [
     30, //beginning of game, water height will be 30 units
     80, //height of water at first offence
@@ -35,9 +30,20 @@ const waterHeights = [
     290,
     380
 ];
-// let wrongAttempts = [];
-let instancesOfLetter;
+let instancesOfLetter; //for calculating the number of times the key pressed was found in the word
 
+/*Following variables are for the sprite animations*/
+let spriteSheet; //spritesheet image of a dancing monkey.
+let spriteData; //spritesheet data for dividing individual images.
+let monkeyImgs = []; //for storing extracted individual images of monkey sprite.
+let charImgs = []; //for storing extracted individual images of character sprite.
+let charIndex = 0; //for animation.
+let monkeyIndex = 0; //for animation.
+let startButton; //for starting the game
+let heightOfWater; //to store the max height of the water drawn using perlin noise 
+let prevHeight; //to store the min height of the water drawn using perlin noise
+
+/*Following variables are for the perlin noise water*/
 let dx = 0.02;
 let inc = 0.002;
 let start = 100;
@@ -53,45 +59,68 @@ function preload() {
 
 function setup() {
     createCanvas(500, 500);
-    bgColour = 220;
     initialization();
 }
 
 
 function draw() {
-    background(bgColour);
+    background(0, 220, 250);
 
-
+    drawGround();
     if (mode === 1) {
-        textSize(80);
+        fill(0);
+        textSize(60);
         text('DROWNMAN', width / 2, height / 2 - 100);
+        noStroke();
+        textSize(10);
+        text('Guess the word with less than ' + attemptsLeft + ' errors\nto save your character from drowning.', width / 2, height / 2 - 70);
+
+        strokeWeight(0.5);
         textSize(20);
         text(
-            'Please enter your name\nThen press the button to play the game',
+            'Please enter your name\n\nThen press the button to start the game',
             width / 2,
-            height / 2 + 100
+            height / 2 + 120
         );
     } else {
-        drawGround();
+        // drawGround();
         if (mode === 1.5) { //mode 1.5 is animation of character into the scene.
             splashScreen();
         } else {
             drawGameCharacter();
-            if (mode === 2) { //mode 2 is the actual game 
+
+            if (mode === 2) {
+                //mode 2 is the actual game, but there's no code in here because the drawing is common with other modes 
+                if (attemptsLeft === 0)
+                    mode = 5; //game is lost
             }
+
             if (mode === 3) { //mode 3 is when the player enters a wrong letter, so the water rises till next level
                 fill(0);
-                text('WRONG!\nOnly ' + (waterHeights.length - wrongAttempts.length - 1) + ' attempts left!', width / 2, 50);
                 animateRisingWater();
+                text('WRONG!\nOnly ' + attemptsLeft + ' attempts left!', width / 2, 50);
             }
+
             if (mode === 4) { //mode 4 is when the player enters a right letter, so two monkeys dance 
                 fill(0);
                 text('CORRECT!\nGood work ' + nameOfPlayer, width / 2, 50);
                 monkeysDance(); //a short animation of two monkey dancing when you enter a correct letter
             }
+
+            if (mode === 5) { //mode 5 is when all attempts are over and game is lost 
+                alert("No more attempts left!\n" + nameOfPlayer + " drowned.\nThe word was '" + word + "'\nPress OK to restart the game");
+                noLoop();
+                window.location = '/';
+            }
+
+            if (mode === 6) {
+                alert("Congratulations " + nameOfPlayer + "!\nYou guessed '" + word + "' right!\nPress OK to restart the game");
+                noLoop();
+                window.location = '/';
+            }
         }
         drawWater();
-        drawWord();
+        drawWords();
     }
 
 }
@@ -109,7 +138,7 @@ function initialization() {
     imageMode(CENTER);
     textFont('Courier');
     textSize(80);
-    charHeight = 0;
+    charY = 0;
     heightOfWater = 0;
     prevHeight = 0;
     instancesOfLetter = 0;
@@ -125,35 +154,36 @@ function initialization() {
     //if the word is not of right length, keep looking for another till it's found
     while (isNotSuitable(word))
         word = random(words);
-    wordChars = word.split('');
+
+    wordChars = word.split(''); //storing characters of word as array for ease of access
     console.log(word);
-    selectedCorrectedChars = [];
+    rightAttempts = [];
     wrongAttempts = [];
+    attemptsLeft = waterHeights.length - 1;
 
     //creating input region
-    input = createInput('');
+    input = createInput(''); //DOM Input element to enter name
     input.size(240, 50);
     input.position(width / 2 - 120, height / 2 - 30);
     input.addClass('box'); //css comes into play here
 
     //creating start button
-    startButton = createButton('Start Game');
+    startButton = createButton('Start Game'); //DOM button to start the game
     startButton.size(100, 40);
     startButton.position(width / 2 - 50, height / 2 + 30);
     startButton.mousePressed(() => {
-        nameOfPlayer = input.value() || 'Player';
+        nameOfPlayer = input.value() || 'Player'; //in case input is left empty
         startButton.hide();
         input.hide();
         mode = 1.5;
-        bgColour = [0, 220, 250]; //sky blue background
     })
 }
 
-function isNotSuitable(w) {
+function isNotSuitable(w) { //for checking if word is of right length
     return (w.length < 4 || w.length > 7);
 }
 
-function drawGround() {
+function drawGround() { //for drawing the green ground and walls
     fill(0, 220, 0);
     noStroke();
     rect(0, maxHeight, 100, 300)
@@ -164,16 +194,17 @@ function drawGround() {
     line(0, 400, width, 400);
 }
 
-function splashScreen() {
-    image(charImgs[0], width / 2 + 100, charHeight, 350, 340); //draw character image
-    charHeight += 4; //animate him coming down
-    if (charHeight >= 245) {
+function splashScreen() { //animation when game is started by pressing button
+    image(charImgs[0], width / 2 + 100, charY, 350, 340); //draw character image
+    charY += 4; //animate him coming down
+    if (charY >= 245) {
         mode = 2;
     }
     if (heightOfWater <= waterHeights[0]) //make sure water level is correct
         heightOfWater += 0.5;
     else
         heightOfWater = waterHeights[0];
+    textFont('Calibri');
 }
 
 function drawWater() {
@@ -182,7 +213,7 @@ function drawWater() {
     v = start;
     beginShape();
     vertex(100, 380);
-    for (let x = 100; x < 400; x++) {
+    for (let x = 100; x <= 400; x++) {
         let y = map(
             noise(v), 0, 1,
             ((380 - prevHeight) > maxHeight) ? 380 - prevHeight : maxHeight, //ternary operator for adding an upper limit to height of water
@@ -198,7 +229,7 @@ function drawWater() {
 
 function drawGameCharacter() {
     line(0, maxHeight, width, maxHeight);
-    image(charImgs[floor(charIndex)], width / 2 + 100, charHeight, 350, 340);
+    image(charImgs[floor(charIndex)], width / 2 + 100, charY, 350, 340);
     charIndex = (charIndex + 0.5) % 15;
 }
 
@@ -211,6 +242,7 @@ function animateRisingWater() {
         prevHeight = waterHeights[wrongAttempts.length - 1] || 0;
         mode = 2; //once the water has risen to the required height, return to normal game mode
     }
+    attemptsLeft = (waterHeights.length - wrongAttempts.length - 1);
 }
 
 function monkeysDance() {
@@ -239,31 +271,35 @@ function keyEntered(key) {
 }
 
 function keyCorrect(i) {
-    selectedCorrectedChars[i] = true;
+    rightAttempts[i] = true;
     instancesOfLetter++;
 }
 
 function keyIncorrect(c) {
-    if (wrongAttempts.indexOf(c)===-1) {
+    if (wrongAttempts.indexOf(c) === -1) { //if the current incorrect key hasnt been entered before, then only penalise
         mode = 3;
         wrongAttempts.push(c);
     }
 }
 
-function drawWord() {
+function drawWords() {
     fill(0);
     noStroke();
-    textSize(20);
+    textSize(30);
     currentWord = [];
     for (let i = 0; i < word.length; i++) {
-        if (selectedCorrectedChars[i])
+        if (rightAttempts[i])
             currentWord[i] = word[i]
         else
             currentWord[i] = '-';
     }
-    currentWord = currentWord.join('');
-    text(currentWord, width / 2, 450);
+    if (currentWord.join('') === word)
+        mode = 6;
 
+    let currentW = currentWord.join(' ');
+    text(currentW, width / 2, 430);
+
+    textSize(20);
     let incorrectLetters = wrongAttempts.join(', ');
-    text(incorrectLetters, width / 2, 480);
+    text("Failed Attempts: " + incorrectLetters, width / 2, 480);
 }
